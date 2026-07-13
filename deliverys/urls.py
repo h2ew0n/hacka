c7cb6c946565984615bccb7c2c261060976824e8
유채영 <ooyoo80@gmail.com>
2026-07-13 18:08:10 +0900

    feat: 백엔드 기본 로직 및 템플릿 파일 구성

---
+++ deliverys/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+app_name = 'deliverys'
+
+urlpatterns = [
+    path('', views.main, name='main'),  # 1. 공통 메인 화면(시작 화면)
+
+    path('learning/mission/', views.learn_mission, name='learn_mission'),   # 2. 미션 화면
+    path('learning/search/', views.learn_search, name='learn_search'),  # 3. 메뉴 검색
+    path('learning/list', views.learn_list, name='learn_list'),     # 4. 식당 리스트
+    path('learning/menu/', views.learn_menu, name='learn_menu'),    # 5. 메뉴 선택
+    path('learning/menu_option/', views.learn_menu_option, name='learn_menu_option'),   # 6. 메뉴 옵션
+    path('learning/cart/', views.learn_cart, name='learn_cart'),    # 7. 장바구니
+    path('learning/payment/', views.learn_payment, name='learn_payment'),   # 8. 결제
+    path('learning/success/', views.learn_success, name='learn_success'),  # 9. 성공 화면
+
+    # 활용 세션 (Application Mode - 추후 구현)
+    path('apply/mission/', views.apply_mission, name='apply_mission'),
+    path('apply/search/', views.apply_search, name='apply_search'),
+    path('apply/menu/', views.apply_menu, name='apply_menu'),
+    path('apply/cart/', views.apply_cart, name='apply_cart'),
+    path('apply/payment/', views.apply_payment, name='apply_payment'),
+    path('apply/success/', views.apply_success, name='apply_success'),
+]
---
+++ deliverys/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main(request):
+    return render(request, 'deliverys/main.html')
+
+# --- 학습 모드 ---
+def learn_mission(request):
+    return render(request, 'deliverys/learn_mission.html')
+
+def learn_search(request):
+    return render(request, 'deliverys/learn_search.html')
+
+def learn_search_detail(request):
+    return render(request, 'deliverys/learn_search_detail.html')
+
+def learn_menu(request):
+    return render(request, 'deliverys/learn_menu.html')
+
+def learn_cart(request):
+    return render(request, 'deliverys/learn_cart.html')
+
+def learn_payment(request):
+    return render(request, 'deliverys/learn_payment.html')
+
+def learn_success(request):
+    return render(request, 'deliverys/learn_success.html')
+
+# --- 활용 모드 (껍데기) ---
+def apply_mission(request): return render(request, 'deliverys/apply_mission.html')
+def apply_search(request): return render(request, 'deliverys/apply_search.html')
+def apply_menu(request): return render(request, 'deliverys/apply_menu.html')
+def apply_cart(request): return render(request, 'deliverys/apply_cart.html')
+def apply_payment(request): return render(request, 'deliverys/apply_payment.html')
+def apply_success(request): return render(request, 'deliverys/apply_success.html')
